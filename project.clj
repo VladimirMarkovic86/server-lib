baa0b5097bb12700cec1bea3654639b1e8fa04c0
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-22 12:51:28 +0100

    Task 50: Implemented sha256 crypt function in utils-lib project, and this function is now used instead of encrypt-password.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.vladimirmarkovic86/server-lib "0.3.18"
+(defproject org.clojars.vladimirmarkovic86/server-lib "0.3.19"
   :description "Server library"
   :url "http://github.com/VladimirMarkovic86/server-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.3"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.4"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.5"]
                  ]
 
