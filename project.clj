63838d508cd9597609779358e6a2450d1d206f3d
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:55:13 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojars.vladimirmarkovic86/server-lib "0.3.33"
+(defproject org.clojars.vladimirmarkovic86/server-lib "0.3.34"
   :description "Server library"
   :url "http://github.com/VladimirMarkovic86/server-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
-                 [org.clojars.vladimirmarkovic86/request-server-lib "0.1.7"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
+                 [org.clojars.vladimirmarkovic86/request-server-lib "0.1.8"]
                  ]
 
   :min-lein-version "2.0.0"
@@ -16,5 +16,7 @@
   :java-source-paths ["src/java"]
   
   :jar-exclusions [#"^public/"
-                   #"^certificate/"])
+                   #"^certificate/"
+                   #"README.md$"
+                   #"LICENSE$"])
 
